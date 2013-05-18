b9c225d7d93dd1013afc419566de39881c485a65
Gabriel Marte <gmarte@fullsail.edu>
2013-05-17 22:29:08 -0400

    Project 2 commit
    Project is unfinished but submitting to avoid another 25% lateness on
    my grade. Will resume work over the weekend and correct all mistakes to
    have it fully functioning by the 3rd weeks project

---
+++ Week 2/Marte_Gabriel_Project 2/Gold/js/json.js
@@ -0,0 +1,224 @@
+json = {
+			"contact01": {
+				"group": ["Group:", "Top Rank"],
+				"fname": ["First Name:", "Gabriel"],
+				"lname": ["Last Name:", "Marte"],
+				"pnum":  ["Phone Number:", "9175553011"],
+				"email": ["Email:", "Gmarte2206@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "11-15-1983"],
+				"style": ["Style:", "Muay Thai"]
+			},
+			"contact02": {
+				"group": ["Group:", "Top Rank"],
+				"fname": ["First Name:", "Yomaly"],
+				"lname": ["Last Name:", "Decruz"],
+				"pnum":  ["Phone Number:", "9175550011"],
+				"email": ["Email:", "yomaly@live.com"],
+				"vip": 	 ["Is a VIP:", "Yes"],
+				"sex":   ["Sex:", "Female"],
+				"bday":  ["Birthday:", "12-09-1983"],
+				"style": ["Style:", "Brazilian Jiu Jitsu"]
+			},
+			"contact03": {
+				"group": ["Group:", "Top Rank"],
+				"fname": ["First Name:", "Jonas"],
+				"lname": ["Last Name:", "Mayo"],
+				"pnum":  ["Phone Number:", "9175553475"],
+				"email": ["Email:", "Jmayo@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "10-12-1968"],
+				"style": ["Style:", "Capoeira"]
+			},
+			"contact04": {
+				"group": ["Group:", "Top Rank"],
+				"fname": ["First Name:", "Lani"],
+				"lname": ["Last Name:", "Acuzar"],
+				"pnum":  ["Phone Number:", "9175558459"],
+				"email": ["Email:", "lacuzar@live.com"],
+				"vip": 	 ["Is a VIP:", "Yes"],
+				"sex":   ["Sex:", "Female"],
+				"bday":  ["Birthday:", "01-09-1981"],
+				"style": ["Style:", "Sambo"]
+			},
+			"contact05": {
+				"group": ["Group:", "BJJ"],
+				"fname": ["First Name:", "Edwin"],
+				"lname": ["Last Name:", "Delgado"],
+				"pnum":  ["Phone Number:", "9175550014"],
+				"email": ["Email:", "Edel@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "10-23-1983"],
+				"rank": ["rank:", "3rd degree"]
+			},
+			"contact06": {
+				"group": ["Group:", "BJJ"],
+				"fname": ["First Name:", "Grace"],
+				"lname": ["Last Name:", "Foley"],
+				"pnum":  ["Phone Number:", "9175553494"],
+				"email": ["Email:", "gfoley@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "female"],
+				"bday":  ["Birthday:", "04-01-1975"],
+				"rank": ["rank:", "1st degree"]
+			},
+			"contact07": {
+				"group": ["Group:", "BJJ"],
+				"fname": ["First Name:", "Kat"],
+				"lname": ["Last Name:", "leia"],
+				"pnum":  ["Phone Number:", "9175556645"],
+				"email": ["Email:", "kleia@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "female"],
+				"bday":  ["Birthday:", "10-12-1968"],
+				"rank": ["rank:", "3rd degree"]
+			},
+			"contact08": {
+				"group": ["Group:", "BJJ"],
+				"fname": ["First Name:", "Patrick"],
+				"lname": ["Last Name:", "Gonzales"],
+				"pnum":  ["Phone Number:", "9175551115"],
+				"email": ["Email:", "Pg@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "09-12-1985"],
+				"rank": ["rank:", "3rd degree"]
+			},
+			"contact09": {
+				"group": ["Group:", "Muay Thai"],
+				"fname": ["First Name:", "Jason"],
+				"lname": ["Last Name:", "Mayo"],
+				"pnum":  ["Phone Number:", "9175543453"],
+				"email": ["Email:", "Jason.mayo@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "11-12-1970"],
+				"rank": ["rank:", "5th degree"]
+			},
+			"contact10": {
+				"group": ["Group:", "Muay Thai"],
+				"fname": ["First Name:", "Coban"],
+				"lname": ["Last Name:", "lokchao"],
+				"pnum":  ["Phone Number:", "9175547568"],
+				"email": ["Email:", "Coban@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "02-02-1956"],
+				"rank": ["rank:", "9th degree"]
+			},
+			"contact11": {
+				"group": ["Group:", "Muay Thai"],
+				"fname": ["First Name:", "Jason"],
+				"lname": ["Last Name:", "Van Oijen"],
+				"pnum":  ["Phone Number:", "9175559239"],
+				"email": ["Email:", "JVO@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "06-22-1976"],
+				"rank": ["rank:", "4th degree"]
+			},
+			"contact12": {
+				"group": ["Group:", "Muay Thai"],
+				"fname": ["First Name:", "Chris"],
+				"lname": ["Last Name:", "Romulo"],
+				"pnum":  ["Phone Number:", "9175550989"],
+				"email": ["Email:", "CROM@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "08-30-1968"],
+				"rank": ["rank:", "6th degree"]
+			},
+			"contact13": {
+				"group": ["Group:", "Sambo"],
+				"fname": ["First Name:", "Mario"],
+				"lname": ["Last Name:", "Salin"],
+				"pnum":  ["Phone Number:", "9175557775"],
+				"email": ["Email:", "MSal@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "12-04-1984"],
+				"rank": ["rank:", "3rd degree"]
+			},
+			"contact14": {
+				"group": ["Group:", "Sambo"],
+				"fname": ["First Name:", "John"],
+				"lname": ["Last Name:", "Durth"],
+				"pnum":  ["Phone Number:", "9175556645"],
+				"email": ["Email:", "Jdurth@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "01-17-1982"],
+				"rank": ["rank:", "1st degree"]
+			},
+			"contact15": {
+				"group": ["Group:", "Sambo"],
+				"fname": ["First Name:", "Kelvin"],
+				"lname": ["Last Name:", "Ramos"],
+				"pnum":  ["Phone Number:", "9175551617"],
+				"email": ["Email:", "kelram@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "11-01-1971"],
+				"rank": ["rank:", "4th degree"]
+			},
+			"contact16": {
+				"group": ["Group:", "Sambo"],
+				"fname": ["First Name:", "Paul"],
+				"lname": ["Last Name:", "Gaston"],
+				"pnum":  ["Phone Number:", "9175558867"],
+				"email": ["Email:", "PGaston@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "03-12-1989"],
+				"rank": ["rank:", "2nd degree"]
+			},
+			"contact17": {
+				"group": ["Group:", "Capoeira"],
+				"fname": ["First Name:", "Luis"],
+				"lname": ["Last Name:", "Velez"],
+				"pnum":  ["Phone Number:", "9175550304"],
+				"email": ["Email:", "LVelez@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "01-12-1970"],
+				"rank": ["rank:", "9th degree"]
+			},
+			"contact18": {
+				"group": ["Group:", "Capoeira"],
+				"fname": ["First Name:", "Daniel"],
+				"lname": ["Last Name:", "Velez"],
+				"pnum":  ["Phone Number:", "9175554435"],
+				"email": ["Email:", "DVel@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "10-31-1985"],
+				"rank": ["rank:", "3rd degree"]
+			},
+			"contact19": {
+				"group": ["Group:", "Capoeira"],
+				"fname": ["First Name:", "Chris"],
+				"lname": ["Last Name:", "Pover"],
+				"pnum":  ["Phone Number:", "9175551143"],
+				"email": ["Email:", "CPove@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "08-21-1991"],
+				"rank": ["rank:", "2nd degree"]
+			},
+			"contact20": {
+				"group": ["Group:", "Capoeira"],
+				"fname": ["First Name:", "Carlos"],
+				"lname": ["Last Name:", "Lunda"],
+				"pnum":  ["Phone Number:", "9175551109"],
+				"email": ["Email:", "Clun@live.com"],
+				"vip": 	 ["Is a VIP:", "No"],
+				"sex":   ["Sex:", "Male"],
+				"bday":  ["Birthday:", "12-17-1981"],
+				"rank": ["rank:", "6th degree"]
+			},
+
+
+
